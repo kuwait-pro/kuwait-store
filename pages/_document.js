4f0758023b0de956989bc0563232fc6dbfb564a5
kuwait-pro <kuwait.projects2026@gmail.com>
2026-02-08 01:00:07 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,22 +5,20 @@ export default function Document() {
   return (
     <Html lang="ar">
       <Head>
-    
-        {/* ارمي كود GA4 و GTM (جزء الـ Head) هنا زي ما هو كوبي بيست */}
-    
-        {<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7Z3PNJVMGV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-7Z3PNJVMGV');
-</script>}
-  
-    </Head>
+        {/* Google Analytics (GA4) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7Z3PNJVMGV"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7Z3PNJVMGV');
+            `,
+          }}
+        />
+      </Head>
       <body>
-        {/* ارمي كود GTM (جزء الـ Body - noscript) هنا */}
         <Main />
         <NextScript />
       </body>
