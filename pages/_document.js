1924f6418cbba534b2da0c9dba2cb9c39c1b49a7
kuwait-pro <kuwait.projects2026@gmail.com>
2026-02-08 00:57:38 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,14 @@
-<!-- Google tag (gtag.js) -->
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ar">
+      <Head>
+    
+        {/* ارمي كود GA4 و GTM (جزء الـ Head) هنا زي ما هو كوبي بيست */}
+    
+        {<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-7Z3PNJVMGV"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -6,4 +16,14 @@
   gtag('js', new Date());
 
   gtag('config', 'G-7Z3PNJVMGV');
-</script>
+</script>}
+  
+    </Head>
+      <body>
+        {/* ارمي كود GTM (جزء الـ Body - noscript) هنا */}
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
